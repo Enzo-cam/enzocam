041ced022a10f672e3d9412b73d72d73f46475d8
EnzoCam7 <camerac09@gmail.com>
2024-01-10 00:30:23 -0300

    Feat: aboutMe and Header components

---
+++ src/Components/AboutMe.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AboutMe = () => {
+  return (
+    <div>
+        <h1 className='text-3xl'>Hi, I'm </h1>
+        <h1 className='text-3xl font-medium'>Enzo Cámera </h1>
+        <h3 className='text-grisOsc text-lg font-normal'>Frontend Developer at ROD</h3>
+    </div>
+  )
+}
+
+export default AboutMe
---
+++ src/Components/Header.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image'
+import React from 'react'
+import logo from '../../public/RYU.png'
+import Link from 'next/link'
+
+const Header = () => {
+  return (
+    <div className='fixed top-0 left-0 w-full bg-white/70 backdrop-blur-lg shadow-sm flex items-center gap-2 px-3 z-50'>
+        <Image 
+            src={logo}
+            alt='logo'
+            width={50}
+            height={50}
+        />
+        <Link href='#' className='text-black font-medium'>About Me</Link>
+        <Link href='#' className='text-black font-medium'>Work</Link>
+        <Link href='#' className='text-black font-medium'>CV</Link>
+        
+    </div>
+  )
+}
+
+export default Header

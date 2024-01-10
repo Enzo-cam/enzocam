fe82c01516d0bd12dc3d5e9846d2439cdf3ec4f0
EnzoCam7 <camerac09@gmail.com>
2024-01-10 00:30:04 -0300

    Layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Header from '@/Components/Header'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +17,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header/>
+        <main className="min-h-screen mt-20 px-3">
+          {children}
+        </main>
+      </body>
     </html>
   )
 }

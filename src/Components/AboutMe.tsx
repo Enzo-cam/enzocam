e9ed5aeb2cc6273422fb16166ee10251f9c9f243
EnzoCam7 <camerac09@gmail.com>
2024-01-17 00:34:19 -0300

    AboutMe component

--- src/Components/AboutMe.tsx
+++ src/Components/AboutMe.tsx
@@ -1,13 +1,23 @@
-import React from 'react'
-
 const AboutMe = () => {
   return (
-    <div>
-        <h1 className='text-3xl'>Hi, I'm </h1>
-        <h1 className='text-3xl font-medium'>Enzo Cámera </h1>
-        <h3 className='text-grisOsc text-xl mt-1 font-normal'>Frontend Developer at <span className='font-semibold'>ROD</span></h3>
+    <div className="bg-grisAbout text-white px-4 py-10">
+      <h1 className="font-semibold text-3xl mb-2">~ About Me</h1>
+      <p className="mb-4 font-light">
+        I've been studing two years about programming, web development and the
+        technologies. Currently I have a year of professional experience as a
+        developer, with interest in creating a product, optimistic, enthusiastic
+        and able to provide experience in development, design, testing and
+        maintenance of software systems.
+      </p>
+      <p className="mb-4 font-light">
+        I originally focused on building as a frontend engineer then shortly I started taking my first steps in backend to became a Fullstack developer.
+      </p>
+      <p className="font-light">
+        Constantly learning and looking for new ways to learn and grow
+        personally and professionally.
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default AboutMe
+export default AboutMe;

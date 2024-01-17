b73bcb7d11e3225f94c19027197855673c5ac648
EnzoCam7 <camerac09@gmail.com>
2024-01-16 23:55:56 -0300

    Feat: contact into home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import AboutMe from "@/Components/AboutMe"
+import Contact from "@/Components/Contact"
 
 export default function Home() {
   return (
     <div>
       <AboutMe />
+
+      <div className="justify-center mx-auto">
+        <Contact />
+      </div>
     </div>
   )
 }

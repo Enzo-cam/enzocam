49b20fa86ee9caeced14173ed6868c84ae2aa759
EnzoCam7 <camerac09@gmail.com>
2024-01-16 23:59:45 -0300

    Contact component

--- src/Components/Contact.tsx
+++ src/Components/Contact.tsx
@@ -1,23 +1,23 @@
 
 import { FaLinkedin, FaGithub } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
-
+import Link from "next/link";
 
 const Contact = () => {
   return (
     <div className="flex gap-2 mt-10">
-        <button className="flex gap-2 items-center border border-black rounded-md p-2 font-medium">
+        <Link href="#" className="flex gap-2 items-center border border-black rounded-md py-2 px-3 font-medium">
             <FaLinkedin />
             Linkedin
-        </button>
-        <button className="flex gap-2 items-center border border-black rounded-md p-2 font-medium">
+        </Link>
+        <Link href="#" className="flex gap-2 items-center border border-black rounded-md py-2 px-3 font-medium">
             <FaGithub />
             GitHub
-        </button>
-        <button className="flex gap-2 items-center border border-black rounded-md p-2 font-medium">
+        </Link>
+        <Link href="#" className="flex gap-2 items-center border border-black rounded-md py-2 px-3 font-medium">
             <MdEmail />
             Gmail
-        </button>
+        </Link>
     </div>
   )
 }

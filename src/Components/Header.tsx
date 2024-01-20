e887028b5578a6e8164504b84ab57df07b680dec
EnzoCam7 <camerac09@gmail.com>
2024-01-20 01:15:07 -0300

    UI changes

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -5,12 +5,12 @@ import Link from 'next/link'
 
 const Header = () => {
   return (
-    <div className='fixed top-0 left-0 w-full bg-white/70 backdrop-blur-lg shadow-sm flex items-center gap-2 px-3 z-50'>
+    <div className='fixed top-0 left-0 w-full bg-white/60 backdrop-blur-md shadow-sm flex items-center gap-2 px-3 z-50'>
         <Image 
             src={logo}
             alt='logo'
-            width={50}
-            height={50}
+            width={40}
+            height={40}
         />
         <Link href='#' className='text-black font-medium'>About Me</Link>
         <Link href='#' className='text-black font-medium'>Work</Link>
--- src/Components/Perfil.tsx
+++ src/Components/Perfil.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Perfil = () => {
   return (
-    <div>
+    <div className=''>
         <h1 className='text-3xl'>Hi, I'm </h1>
         <h1 className='text-3xl font-medium'>Enzo Cámera </h1>
         <h3 className='text-grisOsc text-xl mt-1 font-normal'>Frontend Developer at <span className='font-semibold'>ROD</span></h3>

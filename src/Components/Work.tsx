559760c263f98a25ad032e567db7e7bc5ab400af
EnzoCam7 <camerac09@gmail.com>
2024-01-20 01:15:27 -0300

    Work section and work card

---
+++ src/Components/Work.tsx
@@ -0,0 +1,14 @@
+
+import WorkCard from "./WorkCard"
+
+
+const Work = () => {
+  return (
+    <div className="px-4 py-10">
+        <h1 className="text-3xl font-semibold">Work</h1>
+        <WorkCard />
+    </div>
+  )
+}
+
+export default Work

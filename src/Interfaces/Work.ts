81534869bd129c496cbf5630f407c6b95d585ff0
EnzoCam7 <camerac09@gmail.com>
2024-01-21 21:54:06 -0300

    Feat: description about works and interface

---
+++ src/Interfaces/Work.ts
@@ -0,0 +1,8 @@
+export interface IWork {
+    icon: string;
+    projectName: string;
+    projectDescription: string;
+    technologies: string;
+    services: string;
+    imagesProject: string[];
+}

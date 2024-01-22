f3801836c4777d22623c4235fcfa8b6d6f6e8297
EnzoCam7 <camerac09@gmail.com>
2024-01-22 19:54:49 -0300

    Feat: work information and interface

--- src/Interfaces/Work.ts
+++ src/Interfaces/Work.ts
@@ -3,6 +3,5 @@ export interface IWork {
     projectName: string;
     projectDescription: string;
     technologies: string;
-    services: string;
     imagesProject: string[];
 }
--- utils/workInfo.ts
+++ utils/workInfo.ts
@@ -1,41 +1,39 @@
 import { IWork } from "@/Interfaces/Work";
 
-export const projects: IWork[] = [
+export const workInfo: IWork[] = [
   {
-    icon: "/destapIcon.png",
-    projectName: "iPhone Shop",
+    icon: "/IS.png",
+    projectName: "iPhoneShop",
     projectDescription:
       "Página web enfocada al registro de ventas e ingresos a reparacion de un local comercial dedicado a la venta de productos Apple.",
-    technologies: "Next.js, Node.js, MongoDB, ChakraUI",
-    services: "UI Design, Development, Third-party Integrations, Database Design",
-    imagesProject: ["/destap/image1.jpg", "/destap/image2.jpg"],
-  },
-  {
-    icon: "/destapIcon.png",
-    projectName: "Destap!",
-    projectDescription:
-      "Aplicacion web y mobile para la adquisicion de bebidas alcoholicas en establecimientos.",
     technologies:
-      "Next.js, Nest.js, MongoDB, TailwindCSS, React Native, Sanity CMS",
-    services: "UI Design, Development, Third-party Integrations",
+      "Next.js, Node.js, MongoDB, ChakraUI, UI Design, Development, Third-party Integrations, Database Design",
     imagesProject: ["/destap/image1.jpg", "/destap/image2.jpg"],
   },
   {
-    icon: "/destapIcon.png",
+    icon: "/yagua.png",
     projectName: "Yaguarete Group",
     projectDescription:
-      "Pagina web para control interno de empresa arenera y sus operaciones.",
-    technologies: "Next.js, Node.js, MongoDB, TailwindCSS",
-    services: "Development",
+      "Pagina web para control interno de empresa arenera y sus operaciones. Registros de ventas, gastos, operaciones en la caja.",
+    technologies:
+      "Next.js, Nest.js, MongoDB, TailwindCSS, React Native, Sanity CMS, UI Design, Development, Third-party Integrations",
+    imagesProject: ["/destap/image1.jpg", "/destap/image2.jpg"],
+  },
+  {
+    icon: "/fundacion.png",
+    projectName: "Fundacion 'Mi alma por Bautista'",
+    projectDescription:
+      "Sistema web diseñado para el control y seguimiento de las operaciones dentro de una Fundacion dedicada al tratamiento terapéutico de niños con autismo y afecciones relacionadas.",
+    technologies: "Next.js, Node.js, MongoDB, TailwindCSS, Development, UI Design.",
     imagesProject: ["/yaguarete/image1.jpg", "/yaguarete/image2.jpg"],
   },
   {
-    icon: "/destapIcon.png",
-    projectName: "Siete Mares Tattoo Studio",
+    icon: "/destap.png",
+    projectName: "Destap!",
     projectDescription:
-      "Landing page moderna y responsive para un estudio de tatuadores. La plataforma ofrece una visión estética e informativa al respecto de los profesionales, ejemplos de sus trabajos y también un sistema intuitivo para solicitar turnos con los mismos.",
-    technologies: "Next.js, Node.js, MongoDB, TailwindCSS",
-    services: "Development, UI Design, Database Design, SEO",
+      "Aplicacion web y mobile para la adquisicion de bebidas alcoholicas en establecimientos bailables como también eventos.",
+    technologies:
+      "Next.js, Nest.js, MongoDB, TailwindCSS, FlutterFlow, Sanity CMS, UI Design, Development, Third-party Integrations",
     imagesProject: ["/yaguarete/image1.jpg", "/yaguarete/image2.jpg"],
   },
 ];

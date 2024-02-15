7a2aebb8b92c8c4e1b9a6ab3eb166b1af5e8b6eb
Enzo-cam <camerac09@gmail.com>
2024-02-15 01:07:36 -0300

    Feat: display of growth, challenges and more info at work's page

--- src/app/projects/[name]/page.tsx
+++ src/app/projects/[name]/page.tsx
@@ -21,28 +21,54 @@ const ProjectPage: FC<Props> = ({ params }) => {
   }
 
   return (
-    <div className="px-4 mb-6">
+    <div className="px-4 mb-4">
       {/* Main */}
       <h1 className="text-4xl font-semibold tracking-wide">{project.projectName}</h1>
       <p className="my-16">Aca va una IMAGEN</p>
-      <h3 className="text-3xl font-semibold mb-6">The Project</h3>
-      <h3 className="text-lg text-grisAbout">{project.projectDescription}</h3>
+
+      {/* Project Description */}
+      <div className="mb-10">
+        <h3 className="text-3xl font-semibold mb-4">The Project</h3>
+        <h3 className="text-lg text-grisAbout">{project.projectDescription}</h3>
+      </div>
 
       {/* Details */}
-      <h3 className="text-3xl font-semibold mt-8 mb-6">Technical details</h3>
-      <p className="text-lg mb-4">For this project I use the following technologies: </p>
-      <ul className="px-1">
-        {project.technologies.map(tech => (
-          <li className="flex gap-1 items-center text-lg font-medium" key={tech}>
-            <FaLongArrowAltRight className="text-blue-900" />
-            {tech}
-          </li>
-        ))}
-      </ul>
+      <div className="mt-8 mb-10">
+        <h3 className="text-2xl font-semibold mb-4">Technical details</h3>
+        <p className="text-lg mb-4">For this project I use the following technologies: </p>
+        <ul className="px-1">
+          {project.technologies.map(tech => (
+            <li className="flex gap-1 items-center text-lg font-medium" key={tech}>
+              <FaLongArrowAltRight className="text-blue-900" />
+              {tech}
+            </li>
+          ))}
+        </ul>
+      </div>
 
       {/* Personal Growth */}
-      <h3 className="text-3xl font-semibold mt-8 mb-4">Personal Growth</h3>
+      <div className="mt-8 mb-10">
+        <h3 className="text-2xl font-semibold mb-4">Personal Growth</h3>
+        <p className="">
+          {project.growth}
+        </p>
+      </div>
 
+      {/* Challenges and Solution */}
+      <div className="mt-8 mb-10">
+        <h3 className="text-2xl font-semibold mb-4">Challenges</h3>
+
+        <h4 className="text-xl font-semibold mb-2">Objective</h4>
+        <p className="mb-6">
+          {project.challenge}
+        </p>
+
+        <h4 className="text-xl font-semibold mt-6 mb-2">Solution</h4>
+        <p className="">
+          {project.solution}
+        </p>
+
+      </div>
 
     </div>
   );

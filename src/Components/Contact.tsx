85f373873ca7e11b9c25040e009c490db2f129b1
Enzo-cam <camerac09@gmail.com>
2024-04-30 19:10:22 -0300

    Feat: bottom contact

--- src/Components/Contact.tsx
+++ src/Components/Contact.tsx
@@ -1,25 +1,29 @@
-
-import { FaLinkedin, FaGithub } from "react-icons/fa";
-import { MdEmail } from "react-icons/md";
 import Link from "next/link";
 
 const Contact = () => {
   return (
-    <div className="flex gap-2 mt-10">
-        <Link href="#" className="flex gap-2 items-center border border-black rounded-md py-2 px-3 font-medium">
-            <FaLinkedin />
-            Linkedin
-        </Link>
-        <Link href="#" className="flex gap-2 items-center border border-black rounded-md py-2 px-3 font-medium">
-            <FaGithub />
-            GitHub
-        </Link>
-        <Link href="#" className="flex gap-2 items-center border border-black rounded-md py-2 px-3 font-medium">
-            <MdEmail />
-            Gmail
-        </Link>
+    <div className="px-4 py-4 flex gap-4 flex-col mb-6">
+      <h1 className="text-3xl font-semibold">~ Contact</h1>
+      <p>
+        Want to get in touch or talk about a project/job?
+        <br />
+        Write me at{" "}
+        <Link
+          className="font-semibold text-grisOsc"
+          href="https://www.linkedin.com/in/enzo-camera/"
+        >
+          Linkedin
+        </Link>{" "}
+        or send me an email at{" "}
+        <a
+          className="text-grisOsc font-semibold"
+          href="mailto:enzo.fcam@gmail.com"
+        >
+          Gmail
+        </a>
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;

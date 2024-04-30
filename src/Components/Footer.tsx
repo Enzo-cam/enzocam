dd51340144b62a28078c6cb1eba047d0d7c5979c
Enzo-cam <camerac09@gmail.com>
2024-04-30 19:10:51 -0300

    Footer component

---
+++ src/Components/Footer.tsx
@@ -0,0 +1,27 @@
+'use client'
+import Image from "next/image";
+import logo from "../../public/RYU.png";
+import Link from "next/link";
+import { useState, useEffect } from "react";
+
+const Footer = () => {
+  const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
+
+  useEffect(() => {
+    // Actualizar el año actual cuando el componente se monte
+    setCurrentYear(new Date().getFullYear());
+  }, []);
+
+  return (
+    <div className=" w-full  shadow-sm flex items-right justify-between gap-2 px-3 z-50 pb-4">
+      <p>
+        &copy; {currentYear} Enzo Cámera ——{" "}
+        <span className="text-xl">
+          🇦🇷
+        </span>
+      </p>
+    </div>
+  );
+};
+
+export default Footer;

9cf042cf8156e25df929abfbf686401a95a6047f
Enzo-cam <camerac09@gmail.com>
2024-05-13 00:52:49 -0300

    Experience job info and interface

---
+++ src/Interfaces/CV.ts
@@ -0,0 +1,14 @@
+export interface ICV {
+    icon: string;
+    name: string;
+    experienceDescription: string;
+    technologies: string;
+    experiencePeriod: String;
+    url?: string;
+}
+
+export interface IStack {
+    name: string;
+    technologies: string;
+}
+

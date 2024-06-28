a915ccc73f970b0948d072395c465d57e1d5a934
Enzo-cam <camerac09@gmail.com>
2024-06-28 00:36:59 -0300

    About me

--- src/Components/AboutMe.tsx
+++ src/Components/AboutMe.tsx
@@ -4,20 +4,20 @@ const AboutMe = () => {
       <div className="max-w-screen-lg mx-auto lg:px-20">
         <h1 className="font-semibold text-3xl mb-2">~ About Me</h1>
         <p className="mb-4 font-light">
-          I've been working two years in programming and web development.
-          Currently I have one year as Frontend developer and focusing in
-          Fullstack.
+          I've been working and studying for two years in programming and web
+          development. Currently, I have one year of experience as Frontend
+          developer and focusing on Fullstack.
         </p>
         <p className="mb-4 font-light">
-          In my journey I was adapting and utilizing the latest technologies to
-          develop high-quality products for freelance clients, with optimistic
-          attitude, enthusiastic with solid skills that contribute to the
-          development, design, testing, and maintenance of software systems, pages
-          and application
+          In my journey, I adapted and used the latest technologies to develop
+          high-quality products, with an optimistic attitude, enthusiasm, with
+          solid skills that contribute to the development, design, testing, and
+          maintenance of software systems, pages, and applications
         </p>
         <p className="font-light" id="work">
-          Constantly learning and looking for new ways to learn and grow
-          personally and professionally.
+          Currently, I am working with clients, getting their ideas from scratch
+          to production. Constantly learning and looking for a former job to
+          grow personally and professionally in the area.
         </p>
       </div>
     </div>

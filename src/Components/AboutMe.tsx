7305f5f20e75d70ce0879664568fcf27bd90fc54
Enzo-cam <camerac09@gmail.com>
2024-06-28 12:12:54 -0300

    Fix: width aboutMe section

--- src/Components/AboutMe.tsx
+++ src/Components/AboutMe.tsx
@@ -1,6 +1,6 @@
 const AboutMe = () => {
   return (
-    <div className="bg-grisAbout text-white px-4 py-10 lg:w-screen lg:px-0">
+    <div className="bg-grisAbout text-white px-4 py-10 lg:w-full lg:px-0">
       <div className="max-w-screen-lg mx-auto lg:px-20">
         <h1 className="font-semibold text-3xl mb-2">~ About Me</h1>
         <p className="mb-4 font-light">
